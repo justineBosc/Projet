769c07bce357250983679761bee380ba65b29bdd
Justine Bosc <jbosc@localhost.localdomain>
2016-03-21 20:06:52 +0100

    Add constructor, destructor. Change attributes

--- Simulation.cpp
+++ Simulation.cpp
@@ -13,9 +13,29 @@
 //                                Constructors
 // ===========================================================================
 
+  Simulation::Simulation(int time, int max_T, int max_init_A)
+  {
+    time_=time;
+    state_=0;
+    
+    state_A_T_=new int[max_T*(max_init_A+1)];
+    for(int i=0; i<(max_T*(max_init_A+1)); i++)
+    {
+      state_A_T_[i]=0;
+    }
+    
+    Phase_=nullptr; 
+  }
+
 // ===========================================================================
 //                                 Destructor
 // ===========================================================================
+
+  Simulation::~Simulation(void)
+  {
+    delete [] state_A_T_;
+    state_A_T_=nullptr;
+  }
 
 // ===========================================================================
 //                               Public Methods
